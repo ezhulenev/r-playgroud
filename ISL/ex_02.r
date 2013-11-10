4d2be5e6ea0ec9642dd2b05951b99355b686c4d7
Eugene Zhulenev <eugene.zhulenev@pellucid.com>
2013-11-10 00:07:56 -0500

    ex02

--- ISL/ex_02.r
+++ ISL/ex_02.r
@@ -1,1 +1,8 @@
 college <- read.csv("data/College.csv")
+rownames(college) <- college[,1]
+college <- college[,-1]
+
+Elite = rep("No",nrow(college))
+Elite[college$Top10perc>50] = "Yes"
+Elite = as.factor(Elite)
+college = data.frame(college, Elite)
